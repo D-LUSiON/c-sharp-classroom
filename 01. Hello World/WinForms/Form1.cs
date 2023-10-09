a4455208252de3f071a918c63a83abab3f9c6a47
Lubomir Peikov <l.peikov@gmail.com>
2023-10-09 18:00:02 +0300

    Example of project structure

---
+++ 01. Hello World/WinForms/Form1.cs
@@ -0,0 +1,11 @@
+namespace WinForms {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+            Environment.Exit(255);
+        }
+    }
+}
